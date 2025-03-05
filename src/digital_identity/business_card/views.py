135ced1af901cf56dc62cd7529c6c162978cd2c5
= <=>
2025-03-05 22:26:07 +0300

    Digital Identity 4

--- src/digital_identity/business_card/views.py
+++ src/digital_identity/business_card/views.py
@@ -12,6 +12,8 @@ def business_card_create(request):
         sample_form = SampleForm(request.POST)
         if sample_form.is_valid():
             print(sample_form.cleaned_data)
+            print(dir(sample_form.fields["email"]))
+            print(sample_form.fields["email"].required)
             submitted = True
 
     return render(request, 'business_card/business_card_create.html', {"sample_form": sample_form, "submitted": submitted})
