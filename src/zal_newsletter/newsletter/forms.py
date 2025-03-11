8a66962b5629e2d9190ca370110281a71b9aaaf1
= <=>
2025-03-11 14:03:40 +0300

    Zal Newsletter 3

--- src/zal_newsletter/newsletter/forms.py
+++ src/zal_newsletter/newsletter/forms.py
@@ -6,3 +6,22 @@ class SubscriptionModelForm(forms.ModelForm):
     class Meta:
         model = Subscription
         fields = ['first_name', 'last_name', 'email', 'interests', 'agreed_to_poicies']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Enter your name"
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Enter your name"
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': "Enter your name"
+            }),
+            'agreed_to_poicies': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            }),
+            'interests': forms.CheckboxSelectMultiple()
+        }
